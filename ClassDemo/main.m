164bd0d9221eae195e722c4fa44e5529c4b35d69
Gabriel Acosta <gabriel.acosta@gdla-mac-150112a.local>
2016-04-23 11:42:42 -0500

    Initial Commit

---
+++ ClassDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ClassDemo
+//
+//  Created by Gabriel Acosta on 4/23/16.
+//  Copyright © 2016 Devs and Geeks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
